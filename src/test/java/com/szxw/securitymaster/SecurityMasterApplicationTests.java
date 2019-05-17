590c37ff2da2cb0bfe2bb02825cf1cea1d490c76
leifei99999 <854478026@qq.com>
2019-05-17 10:31:17 +0800

    创建

---
+++ src/test/java/com/szxw/securitymaster/SecurityMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szxw.securitymaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityMasterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
